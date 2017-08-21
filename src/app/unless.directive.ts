0c385f5905d57c5f1cdcbdaf5cdda678dcbd8ddc
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-08-21 18:46:13 -0400

    custom structural directive

--- src/app/unless.directive.ts
+++ src/app/unless.directive.ts
@@ -1,13 +1,17 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appUnless]'
 })
 export class UnlessDirective {
-  @Input() set unless(value) {
-
+  @Input() set appUnless(condition: boolean) {
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vcRef.clear();
+    }
   }
   
-  constructor() { }
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
 
 }
